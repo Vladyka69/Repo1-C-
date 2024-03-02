dac2f92df1255e61ba97fd07b8b3f418f411577d
PibO0122010 <PibO0122010@WIN-JUB8CCCU1GA.DOMAIN.local>
2024-03-02 14:13:48 +0300

    first commit

---
+++ Repo1/Repo1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int year;
+
+        Console.WriteLine("Введите год: ");
+        year = Convert.ToInt32(Console.ReadLine());
+
+        if (year % 4 == 0)
+        {
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                    Console.WriteLine(year + " Год високосный ");
+                else
+                    Console.WriteLine(year + " Год не високосный ");
+            }
+            else
+                Console.WriteLine(year + " Год високосный ");
+        }
+        else
+            Console.WriteLine(year + " Год не високосный");
+    }
+}
